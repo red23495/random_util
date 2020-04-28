c3d6f6fb4c8fb4d5e8a37d300419b5c2e4c21ced
Mamunur Rashid <mamun.rashid.hridoy@gmail.com>
2020-04-29 02:35:41 +0600

    PYTHON>> ADD: binary exponentiation added (aka mod_pow)

--- Python/Algebra/binary_exponentiation.py
+++ Python/Algebra/binary_exponentiation.py
@@ -1,14 +1,24 @@
 
-def bin_pow(base, power):
+def mod_pow(base, power, mod=None):
     """
-    Complexity: O(log n)
-    Note: This function is only used to calculate exponetiation of
-    integer base to positive integer power and will always return an integer value.
+    Params:
+    @base: base number which needs to be multiplied
+    @power: power to which base should be raised
+    @mod: modulas with which number should be mod'ed
+    returns: (base^power)%mod
+    Implements divide and conquere binary exponetiation algorithm
+    Complexity: O(log power) 
     """
+    if mod:
+        base %= mod
     result = 1
     while power > 0:
         if power & 1:
             result *= base
+            if mod:
+                result %= mod
         base *= base
+        if mod:
+            base %= mod
         power //= 2
     return result
--- Python/Algebra/test/test_binary_exponentiation.py
+++ Python/Algebra/test/test_binary_exponentiation.py
@@ -1,25 +1,31 @@
 import unittest
-from ..binary_exponentiation import bin_pow
+from ..binary_exponentiation import mod_pow
 
 
 class BinaryExponentiationTest(unittest.TestCase):
 
     def test_calculates_power_properly(self):
-        self.assertEqual(bin_pow(2, 2), 4)
-        self.assertEqual(bin_pow(1, 10), 1)
-        self.assertEqual(bin_pow(10, 5), 100000)
+        self.assertEqual(mod_pow(2, 2), 4)
+        self.assertEqual(mod_pow(1, 10), 1)
+        self.assertEqual(mod_pow(10, 5), 100000)
     
     def test_calculates_properly_for_big_result(self):
-        self.assertEqual(bin_pow(10, 100000), 10**100000)
+        self.assertEqual(mod_pow(10, 100000), 10**100000)
     
     def test_calculates_zero_when_base_is_zero(self):
-        self.assertEqual(bin_pow(0, 10), 0)
+        self.assertEqual(mod_pow(0, 10), 0)
     
     def test_calculates_one_when_power_is_zero(self):
-        self.assertEqual(bin_pow(200, 0), 1)
+        self.assertEqual(mod_pow(200, 0), 1)
     
     def test_calculates_positive_when_base_negative_and_pow_even(self):
-        self.assertEqual(bin_pow(-2, 4), 16)
+        self.assertEqual(mod_pow(-2, 4), 16)
     
     def test_calculates_negative_when_base_negative_and_pow_odd(self):
-        self.assertEqual(bin_pow(-2, 3), -8)
+        self.assertEqual(mod_pow(-2, 3), -8)
+    
+    def test_mods_result_properly_when_mod_given(self):
+        self.assertEqual(mod_pow(2, 2, 1), 0)
+        self.assertEqual(mod_pow(1, 10, 17), 1)
+        self.assertEqual(mod_pow(10, 5, 15), 10)
+        self.assertEqual(mod_pow(10, 5, 100001), 100000)
